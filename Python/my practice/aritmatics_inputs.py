f22aad793076c073a44186e776f59afdf50d3de8
imnaimur <naimur58co@gmail.com>
2022-02-10 01:11:42 +0600

    aritmatics and input and floor devision

---
+++ Python/my practice/aritmatics_inputs.py
@@ -0,0 +1,18 @@
+#input function convert anything into string
+#so we need to convert the sting into interger number which is called castig in python.
+a =int(input('input in number(A) '))
+b =int(input('input in number(B) '))
+print('A= ',a)
+print('B= ',b)
+s = (a+b)
+print('Sum of A and B= ',s)
+s1=(a-b)
+print('Sub of A and B= ',s1)
+m=(a*b)
+print('Mul of A and B= ',m)
+d=a/b
+print('Div of A and B= ',d)
+p=a**b
+print('A to the power B= ',p)
+f=a//b #"//"means the floor value of a/b means the lower hole nunber value.
+print("the floor value= ",f)

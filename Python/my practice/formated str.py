cbd907f33075d7ec2b248a713b58fd32eb10d198
imnaimur <naimur58co@gmail.com>
2022-02-11 23:01:52 +0600

    formated string

---
+++ Python/my practice/formated str.py
@@ -0,0 +1,7 @@
+n1=20
+n2=30
+print("{n1} + {n2} = ",n1+n2) #with out "f" letter
+print(f"{n1} + {n2} = ",n1+n2) #with f letter
+print(f"{n1} + {n2} = {n1+n2}") # sum in string
+# whenerver we need to print the value in string we need to use f the double quotation
+#and second bracket {variable}

f4c6b7d22b62544926c3233707d035d4fee280de
imnaimur <naimur58co@gmail.com>
2022-02-13 16:33:46 +0600

    my first if condition

---
+++ Python/my practice/if_condition.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter a number: "))
+num2=int(input("Enter a number: "))
+if num1>num2:
+    print(f"{num1} is greater than {num2}")
+else :
+    print(f"{num2} is greater than {num1}")
+print("code executed")

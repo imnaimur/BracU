60e043210f53f929df1317c2de574d14488a4b51
imnaimur <naimur58co@gmail.com>
2022-02-13 16:39:18 +0600

    my first if else condition

--- Python/my practice/if_condition.py
+++ Python/my practice/if_condition.py
@@ -1,5 +1,11 @@
 num1=int(input("Enter a number: "))
 num2=int(input("Enter a number: "))
+"""
+if condition:
+(tab)
+(tab)
+to finish condition use a backspace
+"""
 if num1>num2:
     print(f"{num1} is greater than {num2}")
 else :

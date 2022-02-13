0d2df5cc087e214a92bec33a41c752a0b9d546bc
imnaimur <naimur58co@gmail.com>
2022-02-13 16:50:54 +0600

    if and if else condition

--- Python/my practice/if_else.py
+++ Python/my practice/if_else.py
@@ -1,5 +1,5 @@
 num1=int(input("Enter a number: "))
-#num2=int(input("Enter a number: "))
+num2=int(input("Enter a number: "))
 """
 if condition:
 (tab)
@@ -8,7 +8,8 @@ to finish condition use a backspace
 mane if condition er vitore prottekline er age double space thakle seta if condition er kaj korbe
 ar je line er age space thakbe na sekhane if condition shes hobe
 """
-if num1>10:
-    num1+=10
-    print(num1)
+if num1>num2:
+    print(f"{num1} is greater than {num2}")
+else :
+    print(f"{num2} is greater than {num1}")
 print("code executed")

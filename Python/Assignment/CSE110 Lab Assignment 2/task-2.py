2ea2da189e9744560560ee25eea84ace5e270288
imnaimur <naimur58co@gmail.com>
2022-02-24 16:45:12 +0600

    Ass-2 task-2

---
+++ Python/Assignment/CSE110 Lab Assignment 2/task-2.py
@@ -0,0 +1,8 @@
+favourite_car=input("Enter your Favourite Car name: ")
+printing_number=int(input("Enter Printing Number: "))
+i=0
+while i<printing_number:
+    print(favourite_car)
+    i+=1
+# for counter in range(printing_number):
+#     print(favourite_car)

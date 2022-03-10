c0076b46afe633d1d31dff2360b766fddd10c71b
imnaimur <naimur58co@gmail.com>
2022-03-10 21:17:46 +0600

    ass-3 task-6

---
+++ Python/Assignment/CSE110 Lab assignment 3/task-6.py
@@ -0,0 +1,11 @@
+# string=input("Enter a string: ")
+# inc=0
+# inc for included index 
+# exc for excluded index
+# for count in range(len(string)):
+#     print(string[inc:inc+1:],":",ord(string[inc:inc+1:]))
+#     inc+=1
+#another way
+# i=input()
+# for x in i:
+#   print(x,":",ord(x))

2c7bbb95fb1a3c344c9a64982558c03e96fbf32e
imnaimur <naimur58co@gmail.com>
2022-03-10 21:19:27 +0600

    ass-3 task-12

---
+++ Python/Assignment/CSE110 Lab assignment 3/task-12.py
@@ -0,0 +1,14 @@
+string=input("Enter a string: ")
+val1=0
+val2=0
+i=0
+index=0
+# for element in string:
+#     if element!=",":
+#        val1=string[index]
+#        print(val1)
+#     index+=1
+while i<10:
+    if "," in string:
+        val1=[index]
+        

e5d63da8026f24ebbe2db5b33c4f2803eb813d39
imnaimur <naimur58co@gmail.com>
2022-03-10 21:13:34 +0600

    modified

--- Python/Assignment/CSE110 Lab Assignment 2(loop)/task-11.py
+++ Python/Assignment/CSE110 Lab Assignment 2(loop)/task-11.py
@@ -2,8 +2,9 @@ i=0
 count=0
 number=int(input("Enter a number: "))
 while i<10:
-    print(number%10)
+    print(number%10,end=",")
     number=number//10
-    if number==0:
+    if number//10==0:
+        print(number%10)
         break
     i-=1

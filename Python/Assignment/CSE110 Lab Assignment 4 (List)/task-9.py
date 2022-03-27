ed48579b9389ae4d50e7d7116368c84c24c1d618
imnaimur <naimur58co@gmail.com>
2022-03-27 23:34:36 +0600

    ass-4 task-9 modifided and reformed

--- Python/Assignment/CSE110 Lab Assignment 4 (List)/task-9.py
+++ Python/Assignment/CSE110 Lab Assignment 4 (List)/task-9.py
@@ -1,11 +1,26 @@
-sr=input("Enter element: ")
-sr=sr.split()
+sr=input("Enter element separated by spaces: ")
+# 7 12 4 55 96 2 11 61 33 42
+# sr=sr.split()
 modified_list=[]
 original_list=[]
-for i in range(len(sr)):
-    if int(sr[i])%2==0:
-       modified_list.append(int(sr[i]))
-    original_list.append(int(sr[i]))
-print("Original list: ",original_list)
-print("Modified list",modified_list)
-print(sr)
+start=0
+end=0
+index=0
+# for i in range(len(sr)):
+#     if int(sr[i])%2==0:
+#        modified_list.append(int(sr[i]))
+#     original_list.append(int(sr[i]))
+# print("Original list: ",original_list)
+# print("Modified list",modified_list)
+# print(sr)
+for i in sr:
+    if i==" ":
+        end=index
+        original_list.append(int(sr[start:end]))
+        start=end+1
+    index+=1
+for j in original_list:
+    if j%2!=0:
+        modified_list.append(j)
+print(original_list)
+print(modified_list)

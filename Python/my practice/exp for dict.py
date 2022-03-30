e90f6f4a86bfba45675e4a7ea978d4128446a96a
imnaimur <naimur58co@gmail.com>
2022-03-30 23:51:14 +0600

    dictonary funtions

---
+++ Python/my practice/exp for dict.py
@@ -0,0 +1,13 @@
+dict1={4:"CSE110",9:"CSE111",6:"CSE421",1:"CSE420"}
+print(dict1.keys())
+print(dict1.values())
+print(dict1.items())
+
+for key in dict1.keys():
+    print(key)
+for val in dict1:
+    print(val)
+for val in dict1.values():
+    print(val)
+for val in dict1.items():
+    print(val)

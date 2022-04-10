dbd80dddb329128c7872738dde09aec564c2022a
imnaimur <naimur58co@gmail.com>
2022-04-10 23:36:39 +0600

    Arbitrary keywords arguement

---
+++ Python/my practice/user define functions/Arbitrary_keywords_arg.py
@@ -0,0 +1,6 @@
+def information(name,id,age,address):
+    print("Name is: ",name)
+    print("Id is: ",id)
+    print("Age is: ",age)
+    print("Address is : ",address)
+information("naimur",22101075,22,"Mohakhali")

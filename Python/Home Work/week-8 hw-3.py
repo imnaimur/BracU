a05423b9b8c3899f84cc6cb1f21d5ee4146ffe08
imnaimur <naimur58co@gmail.com>
2022-04-16 06:23:48 +0600

    week-8&9 hw-3 modified

--- Python/Home Work/week-8 hw-3.py
+++ Python/Home Work/week-8 hw-3.py
@@ -1,7 +1,11 @@
-string1=input("Enter string: ")
-list1=[]
-for i in string1:
-    if i not in list1:
-        j=ord(i)
-        list1.append(str(j)+i+str(j))
-print(list1)
+def ascii_char_ascii(string):
+    list1=[]
+    list2=[]
+    for i in string:
+        if i not in list1:
+            list1.append(i)
+            j=ord(i)
+            list2.append(str(j)+i+str(j))
+    print(list2)
+string1=input("Enter a string: ")
+ascii_char_ascii(string1)

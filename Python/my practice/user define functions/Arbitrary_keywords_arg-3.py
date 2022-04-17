814d87703c698097d9a97844c1ca8b284b0ccbe2
imnaimur <naimur58co@gmail.com>
2022-04-17 23:32:25 +0600

    key value pair syntax

---
+++ Python/my practice/user define functions/Arbitrary_keywords_arg-3.py
@@ -0,0 +1,9 @@
+def inform(**var):
+    print(var)
+    for i,j in var.items():
+        print(i,"=",j)
+
+inform(name="dad",id=22101075,age=22,address="pocket khali")
+tuple1=(1,2,3)
+list1=list(tuple1)
+print(list1)

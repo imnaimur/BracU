be3f36db01a49dd083419834ec60475e008416da
Arman <naimur58co@gmail.com>
2022-07-22 23:39:16 +0600

    Shaun vai

--- README.md
+++
@@ -1,1 +0,0 @@
-# My All Practice In CSE
---
+++ legend.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Shaun vai legend \n" ;
+  cout << "Shaun vai boroloks"<< "\n" <<"Treat don foribo";
+  return 0;
+}
